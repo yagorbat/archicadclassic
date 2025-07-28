e3fdf7062ea4a6ec329d69021bd11d95d2c4ee69
Yagorbat <3kx001@gmail.com>
2025-07-28 20:01:32 +0300

    Deploy website - based on 5c323045195a2f91366796ed7899c6b07604369e

---
+++ assets/js/36994c47.a82ce685.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkarchicadclassic=self.webpackChunkarchicadclassic||[]).push([[9858],{5516:c=>{c.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/aba21aa0.79a3de0f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkarchicadclassic=self.webpackChunkarchicadclassic||[]).push([[5742],{7093:c=>{c.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
